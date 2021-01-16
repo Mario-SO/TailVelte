a11b49e9db33322a06664cd57fc3a01d9a5b3ca0
Mario-SO <mariodevmail1010@gmail.com>
2021-01-16 16:43:03 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name;
+</script>
+
+<main class="flex h-screen">
+	<h1 class="text-center text-5xl text-indigo-500 font-sans">Hello {name}!</h1>
+
+</main>
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+  </style>
